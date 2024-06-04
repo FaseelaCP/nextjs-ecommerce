193fa8c601d72883cd14670dc542d4965929145c
Faseela CP <165648430+FaseelaCP@users.noreply.github.com>
2024-06-04 12:53:07 +0400

    Creted notfound page

---
+++ src/app/not-found/page.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+export default function NotFound() {
+  
+  return (
+    <div>
+      <h1>404 - Page Not Found</h1>
+      <p>Sorry, the page you're looking for doesn't exist.</p>
+      
+    </div>
+  );
+}
